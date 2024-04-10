ac752222b6179b7fbec2d7ec7448f249a15a1f45
Je <jeincr@gmail.com>
2024-04-10 16:57:30 +0300

    добавив загрузку зображень

--- NZTCODER/handlers/basic.py
+++ NZTCODER/handlers/basic.py
@@ -2,5 +2,11 @@ from aiogram import Bot
 from aiogram.types import Message
 
 async def get_start(message: Message, bot: Bot):
-    # await bot.send_message(message.from_user.id, f'Привіт {message.from_user.first_name}. Радий тебе бачити')
     await message.answer(f'Привіт {message.from_user.first_name}. Радий тебе бачити')
+    # await bot.send_message(message.from_user.id, f'Привіт {message.from_user.first_name}. Радий тебе бачити')
+
+# Збереження надсилаємих зображень
+async def get_photo(message: Message, bot: Bot):
+    await message.answer(f'Ти відправив картинку! я збережу її собі')
+    file = await bot.get_file(message.photo[-1].file_id)
+    await bot.download_file(file.file_path, 'photo.jpg')
--- NZTCODER/main.py
+++ NZTCODER/main.py
@@ -1,10 +1,11 @@
 # https://www.youtube.com/@nztcoder
-from aiogram import Bot, Dispatcher
 import asyncio
 import logging
+from aiogram import Bot, Dispatcher, F
 
-from handlers.basic import get_start
+from handlers.basic import get_start, get_photo
 from settings import settings
+from aiogram.filters import Command, CommandStart
 
 async def start_bot(bot: Bot):
     await bot.send_message(settings.bots.admin_id, text='Бот запущено!')
@@ -27,7 +28,9 @@ async def start():
     dp.startup.register(start_bot)
     dp.shutdown.register(stop_bot)
 
+    dp.message.register(get_photo, F.photo)
     dp.message.register(get_start)
+    dp.message.register(get_start, CommandStart())
 
     try:
         await dp.start_polling(bot)
